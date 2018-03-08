9f8c897c9371512ad2775acd85a020292ef20aab
Victòria Oliveres <32366044+vicoliveres@users.noreply.github.com>
2018-03-08 15:41:28 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,17 +1,28 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
 #
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+# Read in a page
+html = scraperwiki.scrape("https://www.wagamama.com/restaurants?q=west%20midlands")
 #
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
+# Find something on the page using css selectors
+root = lxml.html.fromstring(html)
+restaurants = root.cssselect("div.content")
+
+for restaurant in restaurants:
+    record = {}
+    record['Name'] = restaurant("h2").text
+    record['Address'] = restaurant("div.address").text
+    record['Postcode'] = restaurant("div.postcode").text
+    record['Company'] = "Wagamama"
+
+    print record, '------------'
+
+    scraperwiki.sqlite.save(['Name'], record)
+
+# Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
 # # An arbitrary query against the database
